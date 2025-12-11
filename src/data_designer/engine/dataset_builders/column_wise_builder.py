1cea2920ea3a069fdf4908d48e0a8c2b91b42d9e
Johnny Greco <jogreco@nvidia.com>
2025-12-10 21:22:27 -0500

    add check (#116)

--- src/data_designer/engine/dataset_builders/column_wise_builder.py
+++ src/data_designer/engine/dataset_builders/column_wise_builder.py
@@ -171,6 +171,8 @@ class ColumnWiseDatasetBuilder:
         max_workers = MAX_CONCURRENCY_PER_NON_LLM_GENERATOR
         if isinstance(generator, WithLLMGeneration):
             max_workers = generator.inference_parameters.max_parallel_requests
+        elif hasattr(generator.config, "max_parallel_requests"):
+            max_workers = generator.config.max_parallel_requests
         self._fan_out_with_threads(generator, max_workers=max_workers)
 
     def _run_full_column_generator(self, generator: ColumnGenerator) -> None:
